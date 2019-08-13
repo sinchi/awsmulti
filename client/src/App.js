1bf4fc09e45aaec137b1bed0394602c194ca722d
ayoub <ayoub.belghar@gmail.com>
2019-08-13 03:31:51 +0000

    change src

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
-            <Link to="/">Homee</Link>
+            <Link to="/">Home</Link>
             <Link to="/otherPage">Other Page</Link>
           </header>
           <div>
